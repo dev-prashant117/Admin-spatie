9026e43885e7c5c9845e142210ce62538b81ac90
Prashant Gaikar <prashantgaikar117@gmail.com>
2024-08-14 16:29:23 +0530

    update route files

--- routes/admin.php
+++ routes/admin.php
@@ -4,12 +4,20 @@ use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
+// Routes for guests (not authenticated admins)
+Route::middleware(['admin_guest'])
+->prefix('/admin/')
+    ->name('admin.')
+    ->group(function() {
+        Route::get('login', [LoginController::class, 'showLoginPage'])->name('login.page');
+        Route::post('login', [LoginController::class, 'login'])->name('login');
+    });
 
-Route::middleware(['admin_guest'])->prefix('/admin/')->name('admin.')->group(function(){
-    Route::get('login',[LoginController::class,'showLoginPage'])->name('login.page');
-    Route::post('login',[LoginController::class,'login'])->name('login');
-});
-Route::middleware(['admin_auth:Admin,teacher'])->prefix('/admin')->name('admin.')->group(function(){
-    Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
-    Route::get('logout',[DashboardController::class,'logout'])->name('logout');
-});
+// Routes for authenticated admins or teachers
+Route::middleware(['admin_auth:Admin,teacher'])
+    ->prefix('/admin')
+    ->name('admin.')
+    ->group(function() {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('logout', [DashboardController::class, 'logout'])->name('logout');
+    });
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('admin', function () {
-//     return view('admin.index');
-// });
+Route::get('admin', function () {
+    return view('admin.index');
+});
 
 require __DIR__.'/admin.php';
