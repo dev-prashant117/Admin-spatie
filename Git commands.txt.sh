37fcfa0a4c130b0d21a0f06465fd25b53681f530
Prashant Gaikar <prashantgaikar117@gmail.com>
2024-08-14 17:10:46 +0530

    git commands

---
+++ Git commands.txt.sh
@@ -0,0 +1,12 @@
+Git commands
+$ git commit  -m "First of Admin Spatie"<-
+$ git status<-
+$ git push -u origin master<-
+$ git config user.email<-
+
+$ eval "$(ssh-agent-s)"<-
+$ ssh-add ~/.ssh/id_rsa<-
+$ ssh-keygen -t ed25519 -C "prashantgaikar117@gmail.com"<-  to generate ssh keygen 
+$ clip < ~/.ssh/id_ed25519.pub<-
+$ git pull origin master<-
+$ git remote add origin git@github.com:dev-prashant117/Admin-spatie.git<- to add branch 
