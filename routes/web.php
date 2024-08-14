fdb2ec9fc74f47b7bcf9ff9040609f6da37b1766
dev-prashant117 <125554697+dev-prashant117@users.noreply.github.com>
2024-08-14 16:18:41 +0530

    Update web.php
    update route file web.php

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('admin', function () {
-    return view('admin.index');
-});
+// Route::get('admin', function () {
+//     return view('admin.index');
+// });
 
 require __DIR__.'/admin.php';
